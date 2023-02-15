852fb5786ba822ba4e3e701fabff54918c5172bb
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-02-15 16:39:45 +0100

    new email

--- src/pages/components/ContactModal.jsx
+++ src/pages/components/ContactModal.jsx
@@ -80,7 +80,7 @@ const ContactModal = ({ buttonStyles }) => {
         `https://formsubmit.co/rebekka.baer@nidosurf.com`,
         requestOptions
       );
-      console.log(result.body);
+      console.log(result);
       setIsLoading(false);
       if (result.status == 200) {
         setSuccessfulSubmission(true);
