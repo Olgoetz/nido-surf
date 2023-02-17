a545c407a373b7c2f4e5839806787803af0912ac
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-02-17 14:44:35 +0100

    env removed from vercel

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -9,14 +9,6 @@ resource "vercel_project" "landing_page" {
 
 }
 
-resource "vercel_project_environment_variable" "email" {
-  project_id = vercel_project.landing_page.id
-  key        = "EMAIL_ENDPOINT"
-  value      = "goetzoliver89@gmail.com"
-  target     = ["production", "development"]
-}
-
-
 resource "vercel_project_domain" "landing_page_redirect" {
   project_id = vercel_project.landing_page.id
   domain     = "surfspiritfun.de"
